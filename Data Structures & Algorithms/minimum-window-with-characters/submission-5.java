b46a952ba875283d79fe99e4b4a413900e3591ac
shane20000603 <73823775+shane20000603@users.noreply.github.com>
2026-04-16 02:31:52 +0800

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int len1 = s.length();
+        int len2 = t.length();
+        String res = "";
+        if(len1 < len2) return "";
+        int left = 0;
+        Map<Character, Integer> map = new HashMap();
+        for(char c: t.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) - 1);
+        }
+        int diff = map.keySet().size();
+        for(int right = 0; right < len1; right++){
+            char c = s.charAt(right);
+            System.out.println("right to: "+c);
+            if(map.containsKey(c)){
+                int num = map.get(c);
+                map.put(c, num + 1);
+                System.out.println("put: "+c);
+                if(num + 1 == 0) diff--;
+            } 
+            if(diff == 0){
+                if(res.length() == 0 || res.length() > right - left + 1){
+                    res = s.substring(left, right + 1);
+                }
+            }
+            while(diff == 0 && left <= right){
+                char leftC = s.charAt(left);
+                System.out.println("shrink from: "+leftC);
+                if(map.containsKey(leftC)){
+                int cNum = map.get(leftC);
+                map.put(leftC, cNum - 1);
+                System.out.println(cNum+ " remove: "+leftC);
+                if(cNum - 1 < 0){
+                    diff++;
+                }}
+                if(res.length() == 0 || res.length() > right - left + 1){
+                    res = s.substring(left, right + 1);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
